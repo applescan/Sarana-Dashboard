0b93e9432875a07607005389e98abd0bd65d81fe
Felicia <feliu.ren@gmail.com>
2024-09-01 00:24:03 +1200

    add background image for login

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,17 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center my-auto h-[80vh]">
-      <SignIn path="/sign-in" afterSignOutUrl="/" />
+    <div
+      style={{
+        backgroundImage: `url("/sign-up.png")`,
+        height: '100%',
+        backgroundRepeat: 'repeat',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="flex justify-center items-center my-auto h-[80vh]">
+        <SignIn path="/sign-in" afterSignOutUrl="/" />
+      </div>
     </div>
   );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,17 @@ import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center">
-      <SignUp path="/sign-up" afterSignOutUrl="/" />
+    <div
+      style={{
+        backgroundImage: `url("/sign-up.png")`,
+        height: '100%',
+        backgroundRepeat: 'repeat',
+        backgroundSize: 'cover',
+      }}
+    >
+      <div className="flex justify-center items-center">
+        <SignUp path="/sign-up" afterSignOutUrl="/" />
+      </div>
     </div>
   );
 }
